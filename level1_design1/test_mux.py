0d6aa28144f21374cfac5a88ba210db38b97320b
Devansh Tanna <devansht0210@gmail.com>
2022-07-20 15:51:54 +0000

    Added test for level1_design1

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -3,8 +3,22 @@
 import cocotb
 from cocotb.triggers import Timer
 
+class DisconnectedPortsError(Exception):
+
+    def __init__(self):
+        super().__init__("There are Disconnected Ports to DUT, kindly refer to module Definition")
+
+
 @cocotb.test()
 async def test_mux(dut):
     """Test for mux2"""
+    inp_val = 0b10
+    num_sel_bits = 5
 
-    cocotb.log.info('##### CTB: Develop your test here ########')
+    for i in range(31):
+        getattr(dut, f"inp{i}").value = inp_val
+        dut.sel.value = i
+        await Timer(2, "ns")
+        dut._log.info(dut.out.value)
+        assert dut.out.value == inp_val, f"Values {inp_val} != {dut.out.value} didn't match"
+
